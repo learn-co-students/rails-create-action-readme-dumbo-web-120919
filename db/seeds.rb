6d14c819119d458d8898f5bb66da56b57e4c40bf
itslizkim <elizabeth.kim93@gmail.com>
2020-01-14 10:25:29 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# create_table :posts do |t|
+#     t.string :title
+#     t.text :description
+
+Post.create(title: 'Hello World', description: 'Hello World')
+Post.create(title: 'What is up', description:'aljdsfajflflaefanks..asld a;jsdf;a')
